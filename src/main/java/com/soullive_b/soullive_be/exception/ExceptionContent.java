77f25840380dfdceb490a11b6c1da0afe3270b30
nohy6630 <nohy6630@gmail.com>
2024-02-29 19:43:37 +0900

    feat: 커스텀 예외 응답에 에러코드 추가

--- src/main/java/com/soullive_b/soullive_be/exception/ExceptionContent.java
+++ src/main/java/com/soullive_b/soullive_be/exception/ExceptionContent.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionContent {
-    NOT_FOUND_USER("유저가 존재하지 않습니다.");
+    NOT_FOUND_USER("유저가 존재하지 않습니다.",1000);
 
     private final String message;
+    private final Integer code;
 }
