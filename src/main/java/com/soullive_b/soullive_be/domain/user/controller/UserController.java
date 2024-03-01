0ddd0bde203c85c45fe8693967fe49cf5b80a18a
김영록 <praoo800@konkuk.ac.kr>
2024-03-01 19:21:21 +0900

    feat : signup controller 구현

--- src/main/java/com/soullive_b/soullive_be/domain/user/controller/UserController.java
+++ src/main/java/com/soullive_b/soullive_be/domain/user/controller/UserController.java
@@ -2,10 +2,14 @@ package com.soullive_b.soullive_be.domain.user.controller;
 
 import com.soullive_b.soullive_be.BaseResponse.BaseResponse;
 import com.soullive_b.soullive_be.argumentResolver.customAnnotation.KakaoId;
+import com.soullive_b.soullive_be.domain.user.request.signup.SignupRequest;
 import com.soullive_b.soullive_be.domain.user.response.login.LoginResponse;
+import com.soullive_b.soullive_be.domain.user.response.signup.SignupResponse;
 import com.soullive_b.soullive_be.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,9 @@ public class UserController {
         return new BaseResponse<>(userService.login(kakaoId));
     }
 
-
+    @PostMapping("/signup")
+    public BaseResponse<SignupResponse> signup(@Validated SignupRequest signupRequest,
+                                               @KakaoId Long kakaoId){
+        return new BaseResponse<>(userService.signup(kakaoId, signupRequest));
+    }
 }
