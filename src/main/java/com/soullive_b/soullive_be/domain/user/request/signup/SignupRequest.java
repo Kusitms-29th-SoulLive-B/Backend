543d04c16c0597c9528c6be64fdd0aff7dc1fe3f
김영록 <praoo800@konkuk.ac.kr>
2024-03-01 19:18:10 +0900

    feat :ã 화원회원가입 request형객체 구현 및 추가

--- src/main/java/com/soullive_b/soullive_be/domain/user/request/signup/SignupRequest.java
+++ src/main/java/com/soullive_b/soullive_be/domain/user/request/signup/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.soullive_b.soullive_be.domain.user.request.signup;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class SignupRequest {
-    @NotBlank
+    @NotBlank(message = "enterprise : 기업명이 비어있으면 안됩니다.")
     private String enterprise;
+    @NotBlank(message = "type : 기업분류가 비어있으면 안됩니다.")
+    private String type;
+    @Email(message = "email : 올바른 메일 형식을 지켜주세요")
+    private String email;
 }
